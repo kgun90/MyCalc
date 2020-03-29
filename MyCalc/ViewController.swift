f471cfdd5b15dc77e4d666ee0f24dc2594fd9840
kgun90 <kgun90@gmail.com>
2020-03-29 16:49:21 +0900

    Initial Commit

---
+++ MyCalc/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MyCalc
+//
+//  Created by Geon Kang on 2020/02/12.
+//  Copyright © 2020 Geon Kang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    var inputNum = 0
+    var resultNum = 0
+    
+    @IBAction func numDown_1(_ sender: Any) {
+    }
+    
+}
