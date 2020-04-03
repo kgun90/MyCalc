3c9c712a75ce946a8f889d53affc89243fd9d380
lizhong99 <2lch4future@gmail.com>
2020-04-03 23:21:36 +0900

    lizhong's cal

--- MyCalc/lizhong/LizhongViewController.swift
+++ MyCalc/lizhong/LizhongViewController.swift
@@ -13,12 +13,80 @@ class LizhongViewController: UIViewController {
     
     @IBOutlet weak var ACButton: UIButton!
     @IBOutlet weak var PlusMinusButton: UIButton!
+    @IBOutlet weak var PercentButton: UIButton!
+    @IBOutlet weak var DivisionButton: UIButton!
+    @IBOutlet weak var SevenButton: UIButton!
+    @IBOutlet weak var EightButton: UIButton!
+    @IBOutlet weak var NineButton: UIButton!
+    @IBOutlet weak var MultiplicationButton: UIButton!
     
+    @IBOutlet weak var FoutButton: UIButton!
+    @IBOutlet weak var FiveButton: UIButton!
+    @IBOutlet weak var SixButton: UIButton!
+    @IBOutlet weak var MinusButton: UIButton!
+    @IBOutlet weak var OneButton: UIButton!
+    @IBOutlet weak var TwoButton: UIButton!
+    @IBOutlet weak var ThreeButton: UIButton!
+    @IBOutlet weak var PlusButton: UIButton!
+    @IBOutlet weak var ZeroButton: UIButton!
+    @IBOutlet weak var PeriodButton: UIButton!
+    @IBOutlet weak var EqualButton: UIButton!
     override func viewDidLoad() {
         ACButton.layer.cornerRadius = 40;
         ACButton.clipsToBounds = true;
         
         PlusMinusButton.layer.cornerRadius = 40;
         PlusMinusButton.clipsToBounds = true;
+        
+        PercentButton.layer.cornerRadius = 40;
+        PercentButton.clipsToBounds = true;
+        
+        DivisionButton.layer.cornerRadius = 40;
+        DivisionButton.clipsToBounds = true;
+        
+        SevenButton.layer.cornerRadius = 40;
+        SevenButton.clipsToBounds = true;
+        
+        EightButton.layer.cornerRadius = 40;
+        EightButton.clipsToBounds = true;
+        
+        NineButton.layer.cornerRadius = 40;
+        NineButton.clipsToBounds = true;
+        
+       MultiplicationButton.layer.cornerRadius = 40;
+        MultiplicationButton.clipsToBounds = true;
+        
+        FoutButton.layer.cornerRadius = 40;
+        FoutButton.clipsToBounds = true;
+        
+        FiveButton.layer.cornerRadius = 40;
+        FiveButton.clipsToBounds = true;
+        
+        SixButton.layer.cornerRadius = 40;
+        SixButton.clipsToBounds = true;
+        
+        MinusButton.layer.cornerRadius = 40;
+        MinusButton.clipsToBounds = true;
+        
+        OneButton.layer.cornerRadius = 40;
+        OneButton.clipsToBounds = true;
+        
+        TwoButton.layer.cornerRadius = 40;
+        TwoButton.clipsToBounds = true;
+        
+        ThreeButton.layer.cornerRadius = 40;
+        ThreeButton.clipsToBounds = true;
+        
+        PlusButton.layer.cornerRadius = 40;
+        PlusButton.clipsToBounds = true;
+        
+        ZeroButton.layer.cornerRadius = 40;
+        ZeroButton.clipsToBounds = true;
+        
+        PeriodButton.layer.cornerRadius = 40;
+        PeriodButton.clipsToBounds = true;
+        
+        EqualButton.layer.cornerRadius = 40;
+        EqualButton.clipsToBounds = true;
     }
 }
