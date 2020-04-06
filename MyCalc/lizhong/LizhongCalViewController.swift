c753b1bc7bfe4914dc76702557b96d046833591f
lizhong99 <2lch4future@gmail.com>
2020-04-07 00:27:53 +0900

    lizhong stack view study

---
+++ MyCalc/lizhong/LizhongCalViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LizhongCalViewController.swift
+//  MyCalc
+//
+//  Created by ChungHyun Lee on 06/04/2020.
+//  Copyright © 2020 Geon Kang. All rights reserved.
+//
+
+import SwiftUI
+
+struct LizhongCalViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LizhongCalViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        LizhongCalViewController()
+    }
+}
