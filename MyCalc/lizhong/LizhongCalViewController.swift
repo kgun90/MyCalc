65f058ea08bb0340ee351377b124a9268e259d4e
lizhong99 <2lch4future@gmail.com>
2020-04-09 00:33:20 +0900

    radius test

--- MyCalc/lizhong/LizhongCalViewController.swift
+++ MyCalc/lizhong/LizhongCalViewController.swift
@@ -10,8 +10,36 @@ import Foundation
 import UIKit
 
 class LizhongCalViewController: UIViewController {
+    
     override func viewDidLoad() {
          
     }
     
+    
 }
+@IBDesignable
+class RoundedButton: UIButton {
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        let radius = min(self.bounds.width, self.bounds.height) / 2
+            self.layer.cornerRadius = radius
+        }
+}
+
+class RoundButton: UIButton {
+    @IBOutlet weak var ParentStackView: UIStackView!
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        for view in ParentStackView.arrangedSubviews{
+            if let button = view as? UIButton{
+                button.layer.cornerRadius = button.frame.height * 0.50
+            }
+        }
+
+    }
+}
+
+
