1d1fbe5ec3c32170a3f4fd1bcf203abb758f4b9a
arthur <arthur@watcha.com>
2020-04-18 16:02:15 +0900

    - 계산기 기능 추가

---
+++ MyCalc/arthur/ArthurCalculatorViewModel.swift
@@ -0,0 +1,102 @@
+//
+//  ArthurCalculatorViewModel.swift
+//  MyCalc
+//
+//  Created by 민쓰 on 16/04/2020.
+//  Copyright © 2020 Geon Kang. All rights reserved.
+//
+
+import Foundation
+
+/// Operation 종류 지정
+private enum Operation {
+    // 초기화
+    case clear
+    // 단항 연산
+    case unary((Double) -> Double)
+    // 이항 연산
+    case binary((Double,Double) -> Double)
+    // ..equal
+    case equal
+}
+
+struct ArthurCalcuatorViewModel {
+    public var result: Double? {
+        didSet {
+            // 모든 연산이 끝나고 나서, 이어서 계속 연산 가능하도록 결과를 피연산자에 저장
+            if result != nil {
+                secondOperand = result
+            }
+        }
+    }
+    /// 피연산자
+    private var secondOperand: Double?
+    /// 연산자
+    private var pendingOperation: PendingBinaryOperation?
+    
+    /// 피연산자와 연산자 임시 저장용
+    private struct PendingBinaryOperation {
+        let firstOperand: Double
+        let operation: (Double, Double) -> Double
+        func perform(with secoundOperand: Double) -> Double {
+            return operation(firstOperand,secoundOperand)
+        }
+    }
+    
+    /// 버튼명에 따라 알맞은 오페레이션 동작 매핑
+    private var operations: [String: Operation] = [
+        "AC":   .clear,
+        "+/-" : .unary({-$0}),
+        "%" :   .unary({$0 / 100}),
+        "+" :   .binary( + ),
+        "−" :   .binary( - ),
+        "×" :   .binary( * ),
+        "/" :   .binary( / ),
+        "=" :   .equal,
+    ]
+    
+    /// 피연산자 지정
+    mutating func setOperand(_ operand: Double) {
+        self.secondOperand = operand
+        result = operand
+    }
+    
+    /// 계산 수행 로직
+    /// - clear: 초기화
+    /// - unarr: 단항연산
+    /// - binary: 이항연산
+    /// - eqaul: 결과 수행
+    ///
+    mutating func performOperation(_ symbol: String) {
+        // 매칭되는 오퍼레이션이 있는지 구분
+        guard let operation = operations[symbol] else {
+            result = 0
+            pendingOperation = nil
+            return
+        }
+        
+        switch operation {
+        /// 초기화
+        case .clear:
+            result = 0
+            pendingOperation = nil
+        /// 단항연산
+        case .unary(let function):
+            if secondOperand != nil {
+                result = function(secondOperand!)
+            }
+        /// 이항연산
+        case .binary(let function):
+            // 이항 연산 인 경우, 먼저 첫 번째 피연산자와 연산자를 기록하십시오.
+            if secondOperand != nil {
+                pendingOperation = PendingBinaryOperation(firstOperand: secondOperand!, operation: function)
+                result = nil
+            }
+        /// 결과 수행
+        case .equal:
+            if pendingOperation != nil && secondOperand != nil {
+                result = pendingOperation?.perform(with: secondOperand!)
+            }
+        }
+    }
+}
--- MyCalc/arthur/ArthurViewController.swift
+++ MyCalc/arthur/ArthurViewController.swift
@@ -10,10 +10,91 @@ import Foundation
 import UIKit
 
 class ArthurViewController: UIViewController {
+
+    /// 텍스트가 노출되는 영역
+    @IBOutlet weak var displayLabel: UILabel!
     
+    private var viewModel = ArthurCalcuatorViewModel()
+    
+    /// 타이핑인지 여부
+    private var userIsInTyping: Bool = false
+    private var displayValue: Double {
+        get {
+            guard let doubleValue = Double(displayLabel.text ?? "") else {
+                return 0
+            }
+            return doubleValue
+        }
+        set {
+            displayLabel.text = String(Int(newValue))
+            
+            // TODO: - 소수점 처리 우짜하지
+//            if Double(Int(newValue)) == newValue {
+//                displayLabel.text = String(Int(newValue))
+//            }else {
+//                displayLabel.text = String(newValue)
+//            }
+        }
+    }
     
 }
 
+// MARK: - UserAction Event
+extension ArthurViewController {
+    
+    /// 숫자 버튼을 터치한 경우 실행
+    @IBAction func toupUpInsideDigitButton(_ sender: UIButton) {
+        
+        guard let digit = sender.currentTitle,
+              let displayedText = displayLabel.text
+        else {
+            return
+        }
+    
+       
+        if userIsInTyping {
+             // '0' 입력하는 케이스 처리
+            if digit == "0" && displayedText == "0" {
+                displayLabel.text = "0"
+            }  else {
+                // 앞에 0이외의 숫자가 입력되어 있으면 이어서 붙힘
+                displayLabel.text = displayedText + digit
+            }
+            return
+        }
+       
+//        if digit == "." {
+//            displayLabel.text = "0."
+//        }else {
+//            displayLabel.text = digit
+//        }
+        userIsInTyping = true
+    }
+    
+    /// 오퍼레이션을 터치한 경우 실행
+    /// 1. 피연산자 설정
+    /// 2. 계산 수행
+    /// 3.`DisplayLabel`에 결과 노출
+    @IBAction func toupUpInsideOperationButton(_ sender: UIButton) {
+        guard let operationTitle = sender.currentTitle else { return }
+        
+        // 타이핑 중인 경우
+        if userIsInTyping {
+            // 1. 피연산자 설정
+            viewModel.setOperand(displayValue)
+            userIsInTyping = false
+        }
+        
+        // 2. 계산 수행
+        viewModel.performOperation(operationTitle)
+        
+        // 3. DisplayLabel에 결과 노출
+        if let result = viewModel.result {
+            displayValue = result
+        }
+    }
+    
+}
 
 class ArthurCircleButton: UIButton {
     
