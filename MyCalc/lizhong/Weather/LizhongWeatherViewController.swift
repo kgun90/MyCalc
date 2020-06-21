bf699f4d0588477d377e1fb5eb95475de143d62c
lizhong99 <62829527+lizhong99@users.noreply.github.com>
2020-06-21 16:08:50 +0900

    lizhong' third

---
+++ MyCalc/lizhong/Weather/LizhongWeatherViewController.swift
@@ -0,0 +1,56 @@
+//
+//  LizhongWeatherController.swift
+//  MyCalc
+//
+//  Created by ChungHyun Lee on 2020/06/20.
+//  Copyright © 2020 Geon Kang. All rights reserved.
+//
+
+import UIKit
+
+class LizhongWeatherViewController : UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        WeatherDataSource.shared.fetchSummary(lat: 37.498206, lon: 127.02761) {
+            [weak self] in self?.completion()
+        }
+    }
+}
+
+/*extension LizhongWeatherViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 0
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            //let cell = tableView.dequeueReusableCell(withIdentifier: //SummaryTableViewCell.identifier, for: indexPath) as! //SummaryTableViewCell
+            
+            //return cell
+            
+           return tableView
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+}
+*/
+extension LizhongWeatherViewController {
+    
+    func completion() {
+        if WeatherDataSource.shared.summary != nil {
+            print("성공")
+                   }
+    }
+}
